b31538e7e5d21222c0d194bd5a175fbb9fa76681
Emulie Chhor <emulie.chhor@hotmail.com>
2020-07-19 12:06:46 -0400

    added hamming distance and rna to protein translation solutions

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ### Rosalind-Solutions
 This repository is my solutions to DNA sequencing problems on ROSALIND.
 
+### Solutions
+
 
---
+++ Solutions/findingMotif.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+string = ''
+substring = ''
+
+def countMotif(string, substring):
+    pos = 0
+    while pos < len(string) - len(substring):
+
+
